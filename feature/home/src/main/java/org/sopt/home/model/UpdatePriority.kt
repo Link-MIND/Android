7343b6bc187dd1933d1fdf2344b842b02d26869f
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-10-27 00:18:55 +0900

    [chore] #174 ktlintformat

--- feature/home/src/main/java/org/sopt/home/model/UpdatePriority.kt
+++ feature/home/src/main/java/org/sopt/home/model/UpdatePriority.kt
@@ -4,11 +4,12 @@ enum class UpdatePriority {
   EMPTY,
   MINOR,
   MAJOR,
-  CRITICAL;
+  CRITICAL,
+  ;
 
   companion object {
     fun toUpdatePriority(value: Int): UpdatePriority {
-      return when(value){
+      return when (value) {
         in 0..1 -> MINOR
         in 2..3 -> MAJOR
         in 4..5 -> CRITICAL
