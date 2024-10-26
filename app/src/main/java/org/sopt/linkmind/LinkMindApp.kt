c3d9aac1200a899daabe8488f92e17bf30d45102
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-10-27 01:03:24 +0900

    [feat] #174 ci

--- app/src/main/java/org/sopt/linkmind/LinkMindApp.kt
+++ app/src/main/java/org/sopt/linkmind/LinkMindApp.kt
@@ -1,12 +1,10 @@
 package org.sopt.linkmind
 
 import android.app.Application
-import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import com.amplitude.android.Amplitude
 import com.amplitude.android.Configuration
 import com.kakao.sdk.common.KakaoSdk
-import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.HiltAndroidApp
 import org.sopt.linkmind.BuildConfig.KAKAO_NATIVE_KEY
 import timber.log.Timber
@@ -25,9 +23,6 @@ class LinkMindApp : Application() {
       ),
     )
     amplitude.track("Sign Up")
-
-    var keyHash = Utility.getKeyHash(this)
-    Log.e("키해시", keyHash)
   }
 
   private fun setTimber() {
