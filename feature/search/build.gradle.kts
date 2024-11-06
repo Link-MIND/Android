43f4acc27fcf7ad51e1313229c8985bde7328b77
sangwook <operawook@gmail.com>
2024-11-07 00:27:18 +0900

    [feat] #183 add search module

---
+++ feature/search/build.gradle.kts
@@ -0,0 +1,19 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+  alias(libs.plugins.linkmind.plugin.feature)
+  alias(libs.plugins.kotlin.android)
+}
+
+android {
+  namespace = "org.sopt.search"
+  buildFeatures {
+    viewBinding = true
+  }
+}
+
+dependencies {
+  implementation(projects.domain.category)
+  implementation(libs.orbit.core)
+  implementation(libs.orbit.viewmodel)
+  implementation(libs.coil)
+}
